3dd30090afb45e4c8a4736b5fec6224409aa8ca4
NithinManupuri <manupurinithin@gmail.com>
2024-03-11 20:04:49 +0530

    file addded

---
+++ namedfunction/namefunc.js
@@ -0,0 +1,5 @@
+function hello(name, id) {
+    return name + " , " + id;
+}
+var ans = hello("nithin", 1);
+console.log(ans);
---
+++ namedfunction/namefunc.ts
@@ -0,0 +1,7 @@
+
+function hello(name:string ,id: number) : string {
+    return  name+ " , "+id; 
+}
+
+let ans=hello("nithin",1);
+console.log(ans);
